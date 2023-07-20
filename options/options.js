e7b73c47d39d85af573be9a8e56a1bc1f8a1af14
Matt Riback <matt.riback@crowdbotics.com>
2023-07-19 22:02:43 -0700

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,15 +1,14 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
-  name: "matthew-prototype-42270",
-  url: "https://matthew-prototype-42270.botics.co",
-  api: "https://matthew-prototype-42270.botics.co/api/v1",
-  screenOptions: {
-    headerShown: false
-  }
-}
+  "name": "matthew-prototype-42270",
+  "url": "https://matthew-prototype-42270.botics.co",
+  "api": "https://matthew-prototype-42270.botics.co/api/v1",
+  "screenOptions": {
+    "headerShown": false
+  },
+  "initialRoute": "aboutTheApp"
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
